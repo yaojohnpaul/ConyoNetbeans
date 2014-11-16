d9b5f2e51c5f601010cd2e5cc7c113e742431096
Evan <evan.livelo@gmail.com>
2014-11-16 09:22:45 +0000

    trial 2

--- src/created/ParseTree/Utos/utos_makeUlit.java
+++ src/created/ParseTree/Utos/utos_makeUlit.java
@@ -16,6 +16,7 @@ public abstract class utos_makeUlit implements created.iNode
         public utos_makeKuha m2;
         public sabi_sabi_for s1;
         public sabi_sabi_for s2;
+        public utos_sabisabi m3;
         public utos_block u;
         
         public makeUlit(utos_makeKuha m1, sabi_sabi_for s1, utos_makeKuha m2, utos_block u)
@@ -24,6 +25,7 @@ public abstract class utos_makeUlit implements created.iNode
             this.m2 = m2;
             this.s1 = s1;
             this.s2 = null;
+            this.m3 = null;
             this.u = u;
         }
         
@@ -33,6 +35,17 @@ public abstract class utos_makeUlit implements created.iNode
             this.m2 = null;
             this.s1 = s1;
             this.s2 = s2;
+            this.m3 = null;
+            this.u = u;
+        }
+        
+        public makeUlit(utos_makeKuha m1, sabi_sabi_for s1, utos_sabisabi m3, utos_block u)
+        {
+            this.m1 = m1;
+            this.m2 = null;
+            this.s1 = s1;
+            this.s2 = s2;
+            this.m3 = m3;
             this.u = u;
         }
         
