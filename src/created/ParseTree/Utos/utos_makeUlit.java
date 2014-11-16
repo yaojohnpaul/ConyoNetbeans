ba015e036c0d40f8312106f8588d1805cda86c14
Evan <evan.livelo@gmail.com>
2014-11-16 11:05:03 +0000

    fixed conyo cup again

--- src/created/ParseTree/Utos/utos_makeUlit.java
+++ src/created/ParseTree/Utos/utos_makeUlit.java
@@ -44,7 +44,7 @@ public abstract class utos_makeUlit implements created.iNode
             this.m1 = m1;
             this.m2 = null;
             this.s1 = s1;
-            this.s2 = s2;
+            this.s2 = null;
             this.m3 = m3;
             this.u = u;
         }
